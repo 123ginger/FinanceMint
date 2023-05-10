4784a0b9889a06165ed7a18af48b2b711f81ae0c
123ginger <bradford.tudor@menloschool.org>
2023-05-09 21:51:43 -0700

    Final upload. Website complete

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+"""
+A fully functioning website that is aimed to address the economic difficulties 
+that come with living in the bay area.
+
+Created by: Bradford Tudor 
+Date: May 2023
+"""
+
 from flask import *
 from database import init_db, db_session
 from models import *
@@ -8,10 +16,15 @@ app = Flask(__name__)
 app.secret_key = "vra8eppuJ9vOYP/ybw=="
 
 
-# homepage
+# homepage functionality
 @app.route("/home")
 def home():
-    return render_template("homepage.html")
+    # If someone is logged in then they can go to the page when submitting the website url 
+    # if they are not then they will be directed to the login page
+    if "username" in session:
+        return render_template("homepage.html")
+    else:
+        return redirect(url_for("login"))
 
 
 # login functionality
@@ -20,11 +33,12 @@ def home():
 def login():
     if request.method=="GET":
         return render_template("login.html")
+    # gets the inputted values from the form and establishes the current user
     elif request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
         session["username"] = username
-        # check if there is a person like this
+        # check if there is already a user with same credentials 
         user = db_session.query(User).where((User.username == username) & (User.password == password)).first()
         if user == None:
             return render_template("login.html")
@@ -32,20 +46,20 @@ def login():
             return redirect(url_for("home"))
 
 
-# signup
+# signup functionality
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    # ask for new username and password, check if unique, then add to database and redirect to new page
     if request.method=="GET":
         return render_template("signup.html")
     elif request.method == "POST":
-        # ask for content
+        # retrieving input from signup form
         username = request.form["username"]
         password = request.form["password"]
         password_check = request.form["password-check"]
-        # check that there are no passwords like that and it matches the check 
+        # check that information is unique and that there is no user with that same username
         user = db_session.query(User).where((User.username == username)).first()
         if (user is None) and (password == password_check):
+            # create new user object, add to database, and set a new current user
             temp = User(username, password)
             db_session.add(temp)
             db_session.commit()
@@ -55,21 +69,24 @@ def signup():
             return redirect(url_for("signup"))      
 
 
-# Forum page
+# Forum functionality
 @app.route("/forum", methods=["GET", "POST"])
 def forum():
+    # user cannot get to these pages unless they are signed in
     if "username" in session:
         if request.method=="GET":
             return render_template("forum.html")
         elif request.method == "POST":
+            # retrieves input from form on forum page
             content = request.form["post-text-area"]
             date = datetime.now().date()
             type = request.form["post-type-select"]
             sort_by = request.form["post-sort-by"]
+            # creates a new post and adds to database
             temp = Post(content, date, type, session["username"])
             db_session.add(temp)
             db_session.commit()
-            # print the posts being made while on page
+            # prints the posts on the page by the special case sorting mechanisms (all, user-contributions, today-posted)
             if sort_by == "all":
                 records = db_session.query(Post).all()
                 return render_template('forum.html', records=records)
@@ -80,19 +97,18 @@ def forum():
                 records = db_session.query(Post).where(Post.date == datetime.now().date())
                 return render_template('forum.html', records=records)
             else:
-                # sort the posts based on what user said
-                records = db_session.query(Post).where((sort_by == Post.type)).all()
+                # if not one of these specific cases then sort normally by comparing the type to our databases type
+                records = db_session.query(Post).where((Post.type == sort_by)).all()
                 return render_template('forum.html', records = records)
     else:
         return redirect(url_for("login"))
 
 
-# How to log out
+# logout functionality
 @app.route("/logout")
 def logout():
     if "username" in session:
         session.pop("username")
-    #flash("You've been logged out", "info")
     return redirect(url_for("login"))
  
 
--- models.py
+++ models.py
@@ -1,17 +1,19 @@
 """
-The file that holds the schema/classes
-that will be used to create objects
-and connect to data tables.
+A fully functioning website that is aimed to address the economic difficulties 
+that come with living in the bay area.
+
+Created by: Bradford Tudor 
+Date: May 2023
 """
 
 from sqlalchemy import ForeignKey, Column, INTEGER, TEXT, DATETIME
 from sqlalchemy.orm import relationship
 from database import Base
 
+# User class
 class User(Base):
     __tablename__ = "users"
 
-    # Columns
     username = Column("username", TEXT, primary_key=True, nullable=False)
     password = Column("password", TEXT, nullable=False)
     # Here I am showing an understanding of relationships
@@ -23,10 +25,10 @@ class User(Base):
             self.password = password
 
 
+# Post class
 class Post(Base):
     __tablename__ = "posts"
 
-    #columns
     id = Column("id", INTEGER, primary_key=True, autoincrement=True)
     content = Column('content', TEXT)
     date = Column("date", TEXT)
@@ -34,7 +36,6 @@ class Post(Base):
     # Here I am showing an understanding of relationships
     user_id = Column(TEXT, ForeignKey('users.username'))
     user = relationship("User", back_populates="posts")
-
     
     def __init__(self, content, date, type, user_id):
             # id auto-increments
